9fe11d44e06922bafac996db157933593c36e859
Ivan Vergés <ivan@platoniq.net>
2021-05-03 22:05:37 +0200

    add test suite

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -7,20 +7,10 @@ require "rails_helper"
 # file should be updated to match any change/bug fix introduced in the core
 checksums = [
   {
-    package: "decidim-admin",
-    files: {
-      "/app/views/decidim/admin/organization_appearance/form/_colors.html.erb" => "725fc77b4c80f885b7b4191a75a06949"
-    }
-  },
-  {
     package: "decidim-core",
     files: {
       # layouts
-      "/app/views/layouts/decidim/_head_extra.html.erb" => "1b8237357754cf519f4e418135f78440",
-      "/app/views/layouts/decidim/_logo.html.erb" => "2713715db652c8107f1fe5f2c4d618b6",
-      "/app/views/layouts/decidim/_mini_footer.html.erb" => "55a9ca723b65b8d9eadb714818a89bb3",
-      # validators
-      "/app/validators/etiquette_validator.rb" => "f7a4a652005385a994208f1ab41c4f08"
+      "/app/views/layouts/decidim/_head_extra.html.erb" => "1b8237357754cf519f4e418135f78440"
     }
   }
 ]
