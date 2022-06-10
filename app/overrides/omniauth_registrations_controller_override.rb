088d28a305a42d6baa9c62d629318ab1724bea77
Ivan Vergés <ivan@pokecode.net>
2022-06-10 23:03:21 +0200

    change message

--- app/overrides/omniauth_registrations_controller_override.rb
+++ app/overrides/omniauth_registrations_controller_override.rb
@@ -5,17 +5,18 @@ module OmniauthRegistrationsControllerOverride
   extend ActiveSupport::Concern
 
   included do
-    before_action :ensure_user_exists, only: :create
+    before_action :ensure_user_exists, only: :google_oauth2
 
     def ensure_user_exists
       form_params = user_params_from_oauth_hash || params[:user]
-      @form = form(OmniauthRegistrationForm).from_params(form_params)
+      @form = form(Decidim::OmniauthRegistrationForm).from_params(form_params)
+      email = @form.email || verified_email
 
-      return if Decidim::User.exists?(email: @form.email || verified_email, organization: current_organization)
+      return if email.present? && Decidim::User.exists?(email: email, organization: current_organization)
 
       # No registration allowed
       Rails.logger.info "WARNING: Attempt to register via OAuth: #{verified_email || form.email}"
-      set_flash_message :alert, :failure, kind: @form.provider.capitalize, reason: "Only existing users are allowed to login"
+      flash[:alert] =  "Sorry, the email #{email} is not registered. Only existing users are allowed to login."
       redirect_to decidim.root_path
     end
   end
