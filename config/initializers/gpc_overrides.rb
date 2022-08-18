de0952751f0f0cdc1c27cdd6a6156df5436162b4
Ivan Vergés <ivan@pokecode.net>
2022-08-18 21:23:15 +0200

    redirect user after invitation

--- config/initializers/gpc_overrides.rb
+++ config/initializers/gpc_overrides.rb
@@ -3,6 +3,8 @@
 Rails.application.config.to_prepare do
   # block registration through Omniauth signin
   Decidim::Devise::OmniauthRegistrationsController.include(OmniauthRegistrationsControllerOverride)
+  # redirect users to interest after acceptin an invitation
+  Decidim::Devise::InvitationsController.include(InvitationsControllerOverride)
 
   # detect custom cards for leadership campaigns
   Decidim::CardHelper.include(CardHelperOverride)
