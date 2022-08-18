0f4501a63de536b31a3154574cf6463b302245fc
Ivan Vergés <ivan@pokecode.net>
2022-08-18 19:31:06 +0200

    ignore exceptions on routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,17 @@ Rails.application.routes.draw do
 
   get "/", to: redirect(Rails.application.routes.url_helpers.watch_race_static_path) if Rails.application.secrets.dig(:gpc, :live, :watch_race).present?
 
-  events = Decidim::Assembly.published.find_by(slug: Rails.application.secrets.dig(:gpc, :assemblies, :leadership_events))
-  if events.present?
-    first = events.components.published.first
-    get "/assemblies/#{events.slug}", to: redirect("/assemblies/#{events.slug}/f/#{first.id}") if first.present?
+  # ignore failure due non connections to database (can happen through precompilation process)
+  # rubocop:disable Lint/SuppressedException:
+  begin
+    events = Decidim::Assembly.published.find_by(slug: Rails.application.secrets.dig(:gpc, :assemblies, :leadership_events))
+    if events.present?
+      first = events.components.published.first
+      get "/assemblies/#{events.slug}", to: redirect("/assemblies/#{events.slug}/f/#{first.id}") if first.present?
+    end
+  rescue StandardError
   end
+  # rubocop:enable Lint/SuppressedException:
 
   mount Decidim::Core::Engine => "/"
 end
