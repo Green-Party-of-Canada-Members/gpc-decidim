0b874aa2adfad0394a3f649da2e0f91e65ccee50
Ivan Vergés <ivan@pokecode.net>
2022-09-02 00:35:38 +0200

    fix private admins+calender / fix invite external

--- app/overrides/invites_controller_override.rb
+++ app/overrides/invites_controller_override.rb
@@ -9,6 +9,8 @@ module InvitesControllerOverride
 
     def ensure_external_invites_allowed
       return unless Rails.application.secrets.dig(:gpc, :disable_external_invites)
+      return if params[:meeting_registration_invite].has_key?(:existing_user) &&
+                Decidim::User.exists?(id: params[:meeting_registration_invite][:user_id], organization: current_organization)
 
       flash[:alert] = I18n.t("gpc.external_invites_disabled")
       redirect_to meeting_registrations_invites_path(meeting)
