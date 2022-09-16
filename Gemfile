886ccfd4bfef22997145ae7d666e9b9201ef0f61
Ivan Vergés <ivan@pokecode.net>
2022-09-16 10:44:31 +0200

    add civicrm mod

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem "decidim-decidim_awesome"
 gem "decidim-direct_verifications"
 gem "decidim-templates", DECIDIM_VERSION
 gem "decidim-term_customizer", git: "https://github.com/mainio/decidim-module-term_customizer", tag: "master"
+gem "decidim-civicrm", git: "https://github.com/Platoniq/decidim-module-civicrm", branch: "main"
 
 gem "bootsnap", "~> 1.7"
 
---
+++ db/migrate/20220914143130_create_decidim_civicrm_groups.decidim_civicrm.rb
@@ -0,0 +1,22 @@
+# This migration comes from decidim_civicrm (originally 20210908095921)
+class CreateDecidimCivicrmGroups < ActiveRecord::Migration[5.2]
+  def change
+    create_table :decidim_civicrm_groups do |t|
+      t.references :decidim_organization, null: false, foreign_key: { to_table: :decidim_organizations }, index: { name: :index_decidim_civicrm_groups_on_decidim_organization_id }
+
+      t.integer :civicrm_group_id, null: false
+      t.integer :civicrm_member_count, default: 0
+
+      t.string :title
+      t.string :description
+      t.jsonb :extra, default: {}
+      
+
+      t.boolean :marked_for_deletion, default: false
+      t.boolean :auto_sync_members, default: false
+
+      t.index ["decidim_organization_id", "civicrm_group_id"], name: "index_unique_civicrm_group_and_organization", unique: true
+      t.timestamps
+    end
+  end
+end
