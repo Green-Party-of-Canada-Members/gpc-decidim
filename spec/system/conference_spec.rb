93e770a325a001e5377097bb72d7b1da47654b7d
Anna Topalidi <t.opalidisanna@gmail.com>
2024-01-05 12:46:54 +0100

    remove extra code

--- spec/system/conference_spec.rb
+++ spec/system/conference_spec.rb
@@ -3,23 +3,6 @@
 require "rails_helper"
 require "selenium/webdriver"
 
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.args << "--explicitly-allowed-ports=#{Capybara.server_port}"
-  options.args << "--headless-new"
-  options.args << "--no-sandbox"
-  options.args << if ENV["BIG_SCREEN_SIZE"].present?
-                    "--window-size=1920,3000"
-                  else
-                    "--window-size=1920,1080"
-                  end
-  options.args << "--ignore-certificate-errors" if ENV["TEST_SSL"]
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    options: options
-  )
-end
 describe "Visit a conference", type: :system do
   let(:organization) { create :organization }
   let(:conference) { create :conference, slug: slug, registrations_enabled: registrations_enabled, organization: organization }
