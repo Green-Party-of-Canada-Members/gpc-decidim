e624804101352b8c925c3912b2284d0f218e6b38
Ivan Vergés <ivan@pokecode.net>
2024-10-07 15:56:08 +0200

    comment wrong cells

--- app/helpers/concerns/card_helper_override.rb
+++ app/helpers/concerns/card_helper_override.rb
@@ -6,15 +6,15 @@ module CardHelperOverride
 
   included do
     def card_for(model, options = {})
-      if contestants_component?(model.try(:component))
-        cell "contestant_m", model
-      elsif leadership_assembly?(model)
-        cell "leadership_m", model
-      elsif model.is_a?(Decidim::Meetings::Meeting) && leadership_assembly?(model.try(:participatory_space))
-        cell "leadership_meeting_m", model
-      else
+      # if contestants_component?(model.try(:component))
+      #   cell "contestant_m", model
+      # elsif leadership_assembly?(model)
+      #   cell "leadership_m", model
+      # elsif model.is_a?(Decidim::Meetings::Meeting) && leadership_assembly?(model.try(:participatory_space))
+      #   cell "leadership_meeting_m", model
+      # else
         cell "decidim/card", model, options
-      end
+      # end
     end
   end
 end
